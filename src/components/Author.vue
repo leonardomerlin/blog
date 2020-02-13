368a3f01d988d7568b6063b3dcc48cfe361d3a00
Leonardo Merlin <leonardo.merlin@gmail.com>
2020-02-13 22:31:12 +0100

    Generate gridsome blog

---
+++ src/components/Author.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="author">
+
+		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="300" height="300" blur="5"/>
+
+		<h1 v-if="showTitle" class="author__site-title">
+			{{ $static.metadata.siteName }}
+		</h1>
+
+		<p class="author__intro">
+			A web developer who likes to learn and share.
+		</p>
+
+		<p class="author__links">
+			<a href="//twitter.com/leonardomerlin">Twitter</a>
+			<a href="//github.com/leonardomerlin">GitHub</a>
+		</p>
+
+	</div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+	props: ['showTitle']
+}
+</script>
+
+<style lang="scss">
+.author {
+	margin: 0 auto;
+	max-width: 500px;
+	text-align: center;
+	padding: calc(var(--space) / 2) 0;
+
+	&__image {
+		border-radius: 100%;
+		width: 150px;
+		height: 150px;
+		margin-bottom: 1em;
+	}
+
+	&__intro {
+		opacity: .8;
+	}
+
+	&__site-title {
+		font-size: 1.5em;
+	}
+
+	&__links {
+		margin-top: -.5em;
+		a {
+			margin: 0 .5em;
+		}
+	}
+}
+</style>
