b2cf6985be7925b9722a6ccd8c153e0b3d695254
Leonardo Merlin <leonardo.merlin@gmail.com>
2020-04-26 01:24:37 +0200

    chore: get pathPrefix from env

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,49 +4,50 @@
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const PATH_PREFIX = process.env.PATH_PREFIX || '/blog';
+
 module.exports = {
-  siteName: 'Leonardo Merlin',
-  siteDescription: 'Leonardo Merlin is a web developer who likes to share and learn.',
-  siteUrl: 'https://leonardomerlin.github.io',
-  pathPrefix: '/blog',
+    siteName: 'Leonardo Merlin',
+    siteDescription: 'Leonardo Merlin is a web developer who likes to share and learn.',
+    siteUrl: 'https://leonardomerlin.github.io',
+    pathPrefix: PATH_PREFIX,
 
-  templates: {
-    Post: '/:title',
-    Tag: '/tag/:id'
-  },
+    templates: {
+        Post: '/:title',
+        Tag: '/tag/:id',
+    },
 
-  plugins: [
-    {
-      // Create posts from markdown files
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'Post',
-        path: 'content/posts/*.md',
-        refs: {
-          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
-          tags: {
-            typeName: 'Tag',
-            create: true
-          }
-        }
-      }
-    },{
-      use: '@gridsome/plugin-google-analytics',
-      options: {
-        id: 'UA-35971148-1'
-      }
-    }
-  ],
+    plugins: [
+        {
+            // Create posts from markdown files
+            use: '@gridsome/source-filesystem',
+            options: {
+                typeName: 'Post',
+                path: 'content/posts/*.md',
+                refs: {
+                    // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+                    tags: {
+                        typeName: 'Tag',
+                        create: true,
+                    },
+                },
+            },
+        },
+        {
+            use: '@gridsome/plugin-google-analytics',
+            options: {
+                id: 'UA-35971148-1',
+            },
+        },
+    ],
 
-  transformers: {
-    //Add markdown support to all file-system sources
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
-    }
-  }
-}
+    transformers: {
+        //Add markdown support to all file-system sources
+        remark: {
+            externalLinksTarget: '_blank',
+            externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+            anchorClassName: 'icon icon-link',
+            plugins: ['@gridsome/remark-prismjs'],
+        },
+    },
+};
